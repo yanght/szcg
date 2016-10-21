7dd72e3d751353cb0e006e92f7ef496a8031a62e
yannis <yannis_123@live.com>
2016-10-21 18:06:21 +0800

    table 样式

--- src/client/Szcg.Web/Content/js/project.js
+++ src/client/Szcg.Web/Content/js/project.js
@@ -400,12 +400,19 @@ project.initDelProjectTable = function (modelcode, buttoncode, nodeid) {
                   }
                ],
                "oLanguage": {
+                   "sProcessing":"正在处理.....",
                    'sSearch': '数据筛选:',
                    "sLengthMenu": "每页显示 _MENU_ 项记录",
                    "sZeroRecords": "没有符合条件的数据...",
                    "sInfo": "当前数据为从第 _START_ 到第 _END_ 项数据；总共有 _TOTAL_ 项记录",
                    "sInfoEmpty": "显示 0 至 0 共 0 项",
-                   "sInfoFiltered": "(_MAX_)"
+                   "sInfoFiltered": "(_MAX_)",
+                   "oPaginate": {
+                       "sFirst": "第一页",
+                       "sPrevious": " 上一页 ",
+                       "sNext": " 下一页 ",
+                       "sLast": " 最后一页 "
+                   }
                }, fnDrawCallback: function () {
                    $(".projectdetail").click(function (e) {
                        utils.dialog(this, "案卷详情", 600, 700);
