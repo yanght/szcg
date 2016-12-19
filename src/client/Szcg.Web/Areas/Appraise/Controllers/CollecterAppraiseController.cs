df4c4b60e15307a7c261c3e626fb0b2f2f3c372f
yannis <yannis_123@live.com>
2016-12-19 14:26:38 +0800

    modify

---
+++ src/client/Szcg.Web/Areas/Appraise/Controllers/CollecterAppraiseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Szcg.Web.Areas.Appraise.Controllers
+{
+    public class CollecterAppraiseController : Controller
+    {
+        //
+        // GET: /Appraise/CollecterAppraise/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
